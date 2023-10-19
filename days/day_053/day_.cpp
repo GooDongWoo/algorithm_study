a23a9aae2dd4f2418466f6ba02c5e8781338a4f9
GooDongWoo <wendy1301@naver.com>
2023-10-20 07:47:52 +0900

    delete each folder day.cpp and add day_.cpp which could start coding easily

---
+++ days/day_053/day_.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+#define LOSE 1
+
+int main() {
+
+	std::ios::sync_with_stdio(false);
+	std::cin.tie(NULL);
+	std::cout.tie(NULL);
+
+	if (LOSE) {
+        using namespace std;
+        
+	}
+	else {
+
+    }
+}
